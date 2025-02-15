770c789407ffe42341f4b37a038759d709f5eb8e
Butch Amaga Imperial <butchamagaimperial2003@gmail.com>
2025-02-15 16:10:23 +0800

    merged the boostrap_custom feature

---
+++ barbakesqueue/notification/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+urlpatterns = [
+    
+]
+
+
+'''
+
+Notification system
+
+1. Notify if the order transaction is approved
+2. Notify if a new message is recived
+3. Notify if a new order is arrived
+
+'''
