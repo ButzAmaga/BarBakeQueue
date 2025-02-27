3b997d5374b1bf896fd827f3b136c8e46fa90889
Butch Amaga Imperial <butchamagaimperial2003@gmail.com>
2025-02-27 20:56:00 +0800

    fixed the chat message for staff and customer

--- barbakesqueue/chat/consumers.py
+++ barbakesqueue/chat/consumers.py
@@ -6,78 +6,6 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 from .models import Message
 from django.db.models import Q
-class ChatConsumer(WebsocketConsumer):
-    
-    def connect(self):
-        
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.group_name = f'chat_{self.room_name}'
-        
-        user = self.scope['user']
-        
-        if not user.is_authenticated:
-            self.close()
-        
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
-            self.channel_name
-        )
-        
-        self.accept()
-
-        
-    
-        
-    
-    
-    
-    def disconnect(self, code):
-        
-        async_to_sync(self.channel_layer.group_discard)(
-            self.group_name, self.channel_name
-        )
-        
-        pass
-        # return super().disconnect(code)
-    
-    def receive(self, text_data=None, bytes_data=None):
-        
-        # convert the json into python dict
-        data_json = json.loads(text_data)
-        
-        # access the message key
-        message = data_json['message']
-        
-        message_instance = Message(sender = self.user)
-        
-        '''
-        # send a message to the user
-        self.send(
-            text_data = json.dumps( { 
-                'type' : 'chat',
-                'message' : message} )
-        )
-        '''
-        
-        async_to_sync(self.channel_layer.group_send)(
-            self.group_name, 
-            { 
-                'type' : 'send_message', # method name to execute,
-                'message' : message, # data to passed in method event variable
-                'message_instance' : message_instance
-            }
-        )  
-        #return super().receive(text_data, bytes_data)
-        
-    
-    def send_message(self, event):
-        message = event['message']
-        
-        self.send(json.dumps({
-            'type': 'chat',
-            'message': message
-        }))    
-
 from django.template.loader import render_to_string
 
 class Async_ChatConsumer(AsyncWebsocketConsumer):
@@ -100,7 +28,7 @@ class Async_ChatConsumer(AsyncWebsocketConsumer):
             await self.close()
         
                     
-        # get the user first user group
+        # get the user user group
         self.user_type = await self.user.groups.afirst()
         
         # add to channel
@@ -114,43 +42,66 @@ class Async_ChatConsumer(AsyncWebsocketConsumer):
         
         '''
             if user is customer then let condition to self.user, if user is staff, decode the user and use it as filter id
+
+            Get the messages from the user
         '''
         
-        if self.user_type.name == 'Customer':
-            user_id = self.user
-        elif self.user_type.name == 'Staff':
-            text = self.room_name
-            user_id = int(text.split("_")[-1])
-            user = await User.objects.aget(id = user_id)
-            
-            # the target user to chat with
-            user_id = user 
         
-        async for message in Message.objects.select_related("user").filter(user = user_id).order_by('created_at'):
+        self.target_user = await self.get_target_user()
+        
+        async for message in Message.objects.select_related("user").filter(user = self.target_user).order_by('created_at'):
             
             '''
                 check if the message is owned by the channel owner, if not then she/he recieved the message
             '''
-            if message.is_channel_sender != False:
-                is_receiver = True
-                sender = 'Barbakesqueue Staff'
-            else:
-                is_receiver = False        
-                sender = self.user
-
+            
+            sender , is_align_end = self.get_sender_and_align_connect(message=message)         
 
             # Render HTML template with message data
             html_content = render_to_string("chat/chat_instance.html", {
                 "message":  message.message,
                 "sender": sender, 
-                "is_receiver" : is_receiver
+                "align_end" : is_align_end
             })
         
             # await asyncio.sleep(1)  # ✅ Yields control to other tasks
             await self.send(text_data=html_content)
         
 
+    def get_sender_and_align_connect(self, message):
+        if self.user_type.name == 'Staff':
+                
+            is_align_end = True
+            sender = 'Barbakesqueue Staff'
+
+            if message.is_channel_sender:
+                is_align_end = False
+                sender = message.user
             
+
+        elif self.user_type.name == 'Customer':
+            
+            is_align_end = False
+            sender = 'Barbakesqueue Staff'
+
+            if message.is_channel_sender:
+                is_align_end = True
+                sender = message.user    
+            
+        return [sender, is_align_end] 
+       
+    async def get_target_user(self):
+        if self.user_type.name == 'Customer':
+            return self.user
+        
+        elif self.user_type.name == 'Staff':
+            ''' Decode the target user using the room name '''
+            text = self.room_name
+            user_id = int(text.split("_")[-1])
+            user = await User.objects.aget(id = user_id)
+            
+            # the target user to chat with
+            return user 
     
     async def disconnect(self, code):
         
@@ -166,49 +117,26 @@ class Async_ChatConsumer(AsyncWebsocketConsumer):
         
        print(data) 
        # store the sender name 
-       data['sender'] = str(self.user)
+       data['sender'] = self.user.id
        
        
        '''
             Save the Message to the database
        '''
-
        
-       if self.user_type.name == "Customer":
-           user = self.user
-           is_channel_sender = True 
-       elif self.user_type.name == 'Staff':
-    
-           is_channel_sender = False
-           
-           '''
-               get the user account of receiver, optimize this by saving it as global
-           '''
-           text = self.room_name
-           user_id = int(text.split("_")[-1])
-           user = await User.objects.aget(id = user_id)
-       
+       if self.target_user == self.user:
+           channel_sender = True 
        else:
-           print('Invalid user')
-           self.close()    
+           channel_sender = False
+           
            
        # save to the database
        message = await database_sync_to_async(Message.objects.create)(  
-            user = user,
+            user = self.target_user,
             message = data['message'],
-            is_channel_sender = is_channel_sender
+            is_channel_sender = channel_sender
        )
        
-       
-       '''
-           check if the message is owned by the channel owner, if not then she/he recieved the message
-       '''
-       if self.user_type.name != 'Customer':
-           is_receiver = True
-           sender = 'Barbakesqueue Staff'
-       else:
-           is_receiver = False        
-           sender = self.user
        
        '''
             group_send only supports JSON-serializable data.
@@ -218,31 +146,40 @@ class Async_ChatConsumer(AsyncWebsocketConsumer):
            { 
                 'type' : 'send_message',
                 'data' : data,
-                'sender_id' : self.user.id
+
            }
        )
        
     async def send_message(self, event):
         '''
             Message, implement the user and determine the is_sender through group permission?
-        
         '''
         data = event['data']
         
-        # add attribute type chat
-        data['type'] = 'chat'
+        # get the sender account 
+        sender = await User.objects.aget(id = data['sender'])
         
-            
-        if event["sender_id"] != self.user.id:
-            is_receiver = True
+        # get message alignment
+        if sender == self.user:
+            # own message
+            is_align_end = True
         else:
-            is_receiver = False        
+            # received message
+            is_align_end = False        
+            
+        # get the sender name
+        group = await sender.groups.afirst()
+        if group.name == 'Customer':
+            sender_name = sender
+        else:
+            sender_name = "Barbakesqueue Staff"
+            
             
         # Render HTML template with message data
         html_content = render_to_string("chat/chat_instance.html", {
             "message": data['message'],
-            "sender": data['sender'], 
-            "is_receiver" : is_receiver
+            "sender": sender_name, 
+            "align_end" : is_align_end
         })
        
         await self.send(
