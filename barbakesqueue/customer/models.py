b25a37a3c787f711cdeffd31f38eca15f883e445
Butch Amaga Imperial <butchamagaimperial2003@gmail.com>
2025-03-07 15:58:09 +0800

    binded the user account to customer entity

--- barbakesqueue/customer/models.py
+++ barbakesqueue/customer/models.py
@@ -10,18 +10,22 @@ gender_choices = (
 )
 
 class customer(models.Model):
+
+    account = models.OneToOneField(User, related_name='account' , on_delete=models.CASCADE, null=True)
+    
     first_name = models.CharField(max_length=50)
     init_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_num = models.CharField(max_length=11)
     home_address = models.CharField(max_length=100, default="Daraga")
-    email_add = models.EmailField(max_length=254)
-    contact_num = models.IntegerField(default="09123456789")
+    email_address = models.EmailField(max_length=254)
+    
     gender = models.CharField(
          max_length=50,
          choices = gender_choices
         )
-    profile_pic = models.ImageField(upload_to="Customer/Profile Picture", null=True, blank=True)
+    
+    profile_pic = models.ImageField(upload_to="Customer/Profile Picture")
 
     def __str__(self):
-        return self.firstname
+        return f'{self.first_name} {self.init_name} {self.last_name}'
