4edd94a36567066f6b5f9e267bb331fa9c175d7e
Butch Amaga Imperial <butchamagaimperial2003@gmail.com>
2025-03-09 15:37:53 +0800

    added the order model and modified the cart model

--- barbakesqueue/order/models.py
+++ barbakesqueue/order/models.py
@@ -7,11 +7,22 @@ from cake.models import *
 class Cart(models.Model):
     customer = models.ForeignKey(customer, related_name="cart_items", on_delete=models.CASCADE)
     cake = models.ForeignKey(Cake, on_delete=models.CASCADE)
-    
     quantity = models.IntegerField()
+    
+    order_id = models.ForeignKey("Order", on_delete=models.DO_NOTHING, null=True, blank=True, default=None) 
+    is_ordered = models.BooleanField(default=False)
     
     date_added = models.DateTimeField(auto_now_add=True)    
     
     
     def __str__(self):
         return f"{self.customer}`s {self.cake}" 
+
+class Order(models.Model):
+    customer = models.ForeignKey(customer, related_name="orders", on_delete=models.CASCADE)
+    
+    date_ordered = models.DateTimeField(auto_now_add=True)    
+    
+    def __str__(self):
+        return f"{self.customer}`s order {self.id}" 
+    
