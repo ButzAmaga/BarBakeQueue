f1f4e1753f9d84abc8f9c35dbfb634cc2fb7c273
Butch Amaga Imperial <butchamagaimperial2003@gmail.com>
2025-05-06 17:14:14 +0800

    created the modal for rating but not commited yet

---
+++ barbakesqueue/rating/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from customer.models import *
+# Create your models here.
+
+
+class Rating_rate_choices(models.IntegerChoices):
+    one = 1, "Very Dissatisfied"
+    two = 2, "Dissatisfied"
+    three = 3, "Neutral or Average"
+    four = 4, "Satisfied"
+    five = 5, "Very Satisfied"
+    
+class Rating(models.Model):
+    customer = models.ForeignKey('customer', related_name='ratings', on_delete=models.CASCADE)
+    cake = models.ForeignKey('Cake', related_name='ratings', on_delete=models.CASCADE)
+    
+    rate = models.IntegerField(choices=Rating_rate_choices, default=1)
+    comment = models.TextField()
+    
+    
